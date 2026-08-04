8a653ea95dcd3f559c0f985bd980940f88a45aaa
ShankarDutta <mr.shankardutta@gmail.com>
2026-08-04 16:33:06 +0530

    feat(config): add CORS setup and environment validation

--- config/env.js
+++ config/env.js
@@ -4,12 +4,19 @@ import { z } from "zod";
 // Load environment variables
 dotenv.config();
 
+const clientUrlSchema =
+	process.env.NODE_ENV === "production" ?
+		z.url({ protocol: /^https$/ }) // HTTPS only
+	:	z.url({ protocol: /^https?$/ }); // HTTP or HTTPS
+
 const envSchema = z.object({
 	PORT: z.coerce.number().int().min(1000).max(65535),
+	CLIENT_URL: clientUrlSchema,
 });
 
 const envVars = {
 	PORT: process.env.PORT,
+	CLIENT_URL: process.env.CLIENT_URL,
 };
 
 export const env = envSchema.parse(envVars);
--- server.js
+++ server.js
@@ -1,4 +1,6 @@
+import cors from "cors";
 import express from "express";
+import corsOption from "./config/corsOption.js";
 import { env } from "./config/env.js";
 
 // Initialize Express application
@@ -6,6 +8,11 @@ const app = express();
 
 // Define server port
 const port = env.PORT;
+
+// Middlewares
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(cors(corsOption));
 
 // Start the server
 app.listen(port, () => {
